19472fdfd5c96615c85d60c7c07ab7a4be2b2d71
Ewout Van de Capelle <ewoutvandecapelle@gmail.com>
2015-08-13 22:34:36 +0200

    LogResponder: Add basic logging to file

--- BlinkyBot/Responders/LogResponder.cs
+++ BlinkyBot/Responders/LogResponder.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using MargieBot.Models;
 using System.Diagnostics;
+using System;
+using System.IO;
 
 namespace BlinkyBot.Responders
 {
     class LogResponder : MargieBot.Responders.IResponder
     {
-        //TODO: write lines to file/DB instead of debug
+        //TODO: write lines to DB instead of debug/file
         //TODO: handle message editing, url's, etc...
-
 
         public bool CanRespond(ResponseContext context)
         {
@@ -23,11 +20,19 @@ namespace BlinkyBot.Responders
         {
             Debug.WriteLine("Raw json data: "+context.Message.RawData);
 
-            Debug.WriteLine(context.Message.ChatHub.Name //Name includes #
-                + " [" + context.Message.TimeStamp.ToString("dd/MM/yyyy hh:mm:ss") + "] "
+            //Timezone adjustments should be done by the frontend
+            string logmsg = " [" + context.Message.TimeStamp.ToString("dd/MM/yyyy hh:mm:ss") + "] "
                 + "<" + context.UserNameCache[context.Message.User.ID] + "> "
-                + context.Message.Text);
-            
+                + context.Message.Text;
+
+            Debug.WriteLine(context.Message.ChatHub.Name //Name includes #
+                + logmsg);
+
+            using (StreamWriter outfile = File.AppendText(context.Message.ChatHub.Name + "_" + context.Message.TimeStamp.ToString("dd-MM-yyyy") + ".txt"))
+            {
+                outfile.WriteLine(logmsg);
+            }
+
             return new BotMessage(); //empty response doesn't say anything
         }
     }
