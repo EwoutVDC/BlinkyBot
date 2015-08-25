8ab9d24309bd3e9de8b146c489d158479d2c816c
Ewout Van de Capelle <ewoutvandecapelle@gmail.com>
2015-08-25 16:27:18 +0200

    Change QuoteResponder to use SQLite DB instead of in-memory quotes

---
+++ BlinkyBot/Models/Quote.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using MargieBot.Models;
+
+namespace BlinkyBot.Models
+{
+    class Quote
+    {
+        public long id { get; set; }
+        public string channel { get; set; }
+        public DateTime timestamp { get; set; }
+        public string user { get; set; }
+        public string text { get; set; }
+
+        public Quote() { id = -1; }
+
+        public void setTimestamp(long unixTimeStamp)
+        {
+            timestamp = DateTimeOffset.FromUnixTimeSeconds(unixTimeStamp).DateTime;
+        }
+
+        public string ToString(TimeZoneInfo timezone)
+        {
+            return "Quote #" + id + ": '" + text + "' (Added by " + user
+                    + " at " + TimeZoneInfo.ConvertTimeFromUtc(timestamp, timezone).ToString("dd/MM/yyyy HH:mm:ss")
+                    + "in channel '" + channel + "')";
+        }
+    }
+}
--- BlinkyBot/Responders/QuoteResponder.cs
+++ BlinkyBot/Responders/QuoteResponder.cs
@@ -3,12 +3,13 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using MargieBot.Models;
-
+using BlinkyBot.Models;
 
 namespace BlinkyBot.Responders
 {
     class QuoteResponder : MargieBot.Responders.IResponder
     {
+        public static TimeZoneInfo quoteTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
         public const string quoteStart = "!quote";
         public const string quoteAdd = "add";
         public const string quoteDel = "del";
@@ -16,38 +17,29 @@ namespace BlinkyBot.Responders
         public const string quoteSearch = "search"; //TODO
         public const string quoteRandom = "";
         public const string quoteHelp = "help"; //TODO
-
-        private List<string> quotes = new List<string>();
+        
         private SQLiteConnection dbConn;
 
         public QuoteResponder(SQLiteConnection dbConn)
         {
             this.dbConn = dbConn;
-        }
-
-
-        private int GetQuoteIndex(string request, BotMessage reply)
-        {
-            string idStr = request.Substring(request.IndexOf(" ") + 1);
-            int id = -1;
 
             try
             {
-                id = Convert.ToInt32(idStr);
-            }
-            catch (FormatException)
-            {
-                reply.Text = "Badly formatted id: '" + idStr + "'";
-                return -1;
-            }
+                SQLiteCommand cmd = new SQLiteCommand(
+                    "CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY, channel TEXT, timestamp INTEGER, user TEXT, text TEXT)",
+                    dbConn);
+                cmd.ExecuteNonQuery();
 
-            if (id < 1 || id >= quotes.Count)
-            {
-                reply.Text = "I don't have a quote #" + id;
-                return -1;
+                //TODO: Add indexing to quotes table
             }
-
-            return id-1; //quote id = index + 1
+            catch (InvalidOperationException e)
+            {
+                Debug.WriteLine("InvalidOperationException caught: " + e.ToString());
+                Console.WriteLine("QuoteResponder could not create quotes table");
+                Program.WaitAndExit(1);
+            }
+            
         }
 
         public bool CanRespond(ResponseContext context)
@@ -60,7 +52,6 @@ namespace BlinkyBot.Responders
 
         public BotMessage GetResponse(ResponseContext context)
         {
-
             string request = context.Message.Text.Length > quoteStart.Length ? context.Message.Text.Substring(quoteStart.Length+1) : "";
             Debug.WriteLine("request: '" + request + "'");
             BotMessage reply = new BotMessage() { ChatHub = context.Message.ChatHub };
@@ -71,29 +62,51 @@ namespace BlinkyBot.Responders
             switch (command)
             {
                 case quoteAdd:
-                    string quote = request.Substring(request.IndexOf(" ") + 1)
-                        + " (Added by " + context.UserNameCache[context.Message.User.ID]
-                        + " at " + context.Message.TimeStamp.ToString("dd/MM/yyyy hh:mm:ss") + ")";
-                    if (quote.Length > 0)
                     {
-                        quotes.Add(quote);
-                        reply.Text = "Success! Added quote #" + quotes.Count + ": " + quote;
+                        string quoteText = request.Substring(request.IndexOf(" ") + 1);
+
+                        if (quoteText.Length > 0)
+                        {
+                            Quote quote = new Quote
+                            {
+                                channel = context.Message.ChatHub.Name,
+                                user = context.UserNameCache[context.Message.User.ID],
+                                timestamp = context.Message.TimeStamp,
+                                text = quoteText
+                            };
+
+                            if (addQuoteToDb(ref quote))
+                            {
+                                reply.Text = "Success! Added " + quote.ToString(quoteTimeZone);
+                            }
+                            else
+                            {
+                                reply.Text = "Failed to add " + quote.ToString(quoteTimeZone);
+                            }
+                        }
+                        else
+                        {
+                            reply.Text = "Nope! Not adding empty quote";
+                        }
+
+                        break;
                     }
-                    else
-                    {
-                        reply.Text = "Nope! Not adding empty quote.";
-                    }
-                            
-                    break;
 
                 case quoteDel:
                     {
-                        int id = GetQuoteIndex(request, reply);
+                        long id = GetRequestQuoteId(request, reply);
 
                         if (id != -1)
                         {
-                            reply.Text = "Succesfully deleted quote #" + id + " '" + quotes[id] + "'";
-                            quotes.RemoveAt(id);
+                            Quote deletedQuote = delQuoteFromDb(id);
+                            if (deletedQuote != null)
+                            {
+                                reply.Text = "Succesfully deleted " + deletedQuote.ToString(quoteTimeZone);
+                            }
+                            else
+                            {
+                                reply.Text = "Could not delete quote #" + id;
+                            }
                         }
 
                         break;
@@ -101,28 +114,173 @@ namespace BlinkyBot.Responders
 
                 case quoteGet:
                     {
-                        int id = GetQuoteIndex(request, reply);
-                        if (id != -1)
+                        long id = GetRequestQuoteId(request, reply);
+                        Quote quote = getQuoteById(id);
+                        if (quote != null)
                         {
-                            reply.Text = "Quote #" + id + " " + quotes[id];
+                            reply.Text = quote.ToString(quoteTimeZone);
+                        }
+                        else
+                        {
+                            reply.Text = "Could not find quote #" + id;
                         }
                         break;
                     }
 
                 case quoteRandom:
                     {
-                        Random rnd = new Random();
-                        int id = rnd.Next(0, quotes.Count);
-                        reply.Text = "Quote #" + id + " " + quotes[id];
+                        Quote quote = getRandomQuote();
+                        if (quote != null)
+                        {
+                            reply.Text = quote.ToString(quoteTimeZone);
+                        }
+                        else
+                        {
+                            reply.Text = "Could not get random quote";
+                        }
                         break;
                     }
 
                 default:
                     Debug.WriteLine("Unsupported quote command '" + command + "' (request '" + request + "')");
+                    reply.Text = "Unsupported quote command '" + command + "' (request '" + request + "')";
                     break;
             }
 
             return reply;
         }
+
+        private long GetRequestQuoteId(string request, BotMessage reply)
+        {
+            string idStr = request.Substring(request.IndexOf(" ") + 1);
+            long id = -1;
+
+            try
+            {
+                id = Convert.ToInt64(idStr);
+            }
+            catch (FormatException)
+            {
+                reply.Text = "Badly formatted id: '" + idStr + "'";
+                return -1;
+            }
+
+            return id;
+        }
+
+        private bool addQuoteToDb(ref Quote quote)
+        {
+            quote.id = -1;
+            try
+            {
+                SQLiteCommand cmd = new SQLiteCommand(
+                      "INSERT INTO quotes (channel, timestamp, user, text) values ("
+                      + "@channelname, @timestamp, @username, @text);"
+                      + " SELECT last_insert_rowid()",
+                      dbConn);
+                cmd.Parameters.AddWithValue("@channelname", quote.channel);
+                //No timezone adjustment for the database, this should be done in the frontend (for the local timezone since some weirdos don't live in CET)
+                cmd.Parameters.AddWithValue("@timestamp", (new DateTimeOffset(quote.timestamp)).ToUnixTimeSeconds());
+                cmd.Parameters.AddWithValue("@username", quote.user);
+                cmd.Parameters.AddWithValue("@text", quote.text);
+
+                Debug.WriteLine("Executing sql:");
+                Debug.WriteLine(cmd.CommandText);
+                quote.id = (long)cmd.ExecuteScalar();
+            }
+            catch (InvalidOperationException e)
+            {
+                Debug.WriteLine("InvalidOperationException caught: " + e.ToString());
+            }
+            return quote.id != -1;
+        }
+
+        private Quote delQuoteFromDb(long quoteId)
+        {
+            try
+            {
+                Quote delQuote = getQuoteById(quoteId);
+                if (delQuote != null)
+                {
+                    SQLiteCommand cmd = new SQLiteCommand(
+                          "DELETE FROM quotes WHERE id = @id",
+                          dbConn);
+                    cmd.Parameters.AddWithValue("@id", quoteId);
+
+                    Debug.WriteLine("Executing sql:");
+                    Debug.WriteLine(cmd.CommandText);
+                    int affectedRows = cmd.ExecuteNonQuery();
+                    if (affectedRows != 0)
+                        return delQuote;
+                }
+                Debug.WriteLine("Could not find quote #" + quoteId);
+            }
+            catch (InvalidOperationException e)
+            {
+                Debug.WriteLine("InvalidOperationException caught: " + e.ToString());
+            }
+            return default(Quote);
+        }
+
+        private Quote getQuoteById(long quoteId)
+        {
+            try
+            {
+                SQLiteCommand cmd = new SQLiteCommand(
+                        "SELECT * FROM quotes WHERE id = @id",
+                        dbConn);
+                cmd.Parameters.AddWithValue("@id", quoteId);
+
+                Debug.WriteLine("Executing sql:");
+                Debug.WriteLine(cmd.CommandText);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Quote quote = new Quote();
+                    quote.id = (long)reader["id"];
+                    quote.channel = (string)reader["channel"];
+                    quote.setTimestamp((long)reader["timestamp"]);
+                    quote.user = (string)reader["user"];
+                    quote.text = (string)reader["text"];
+                    return quote;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Debug.WriteLine("InvalidOperationException caught: " + e.ToString());
+            }
+            return default(Quote);
+        }
+
+        private Quote getRandomQuote()
+        {
+            try
+            {
+                SQLiteCommand cmd = new SQLiteCommand(
+                        "SELECT * FROM quotes ORDER BY RANDOM() LIMIT 1",
+                        dbConn);
+
+                Debug.WriteLine("Executing sql:");
+                Debug.WriteLine(cmd.CommandText);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Quote quote = new Quote();
+                    quote.id = (long)reader["id"];
+                    quote.channel = (string)reader["channel"];
+                    quote.setTimestamp((long)reader["timestamp"]);
+                    quote.user = (string)reader["user"];
+                    quote.text = (string)reader["text"];
+                    return quote;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Debug.WriteLine("InvalidOperationException caught: " + e.ToString());
+            }
+            return default(Quote);
+        }
+
+
     }
 }
